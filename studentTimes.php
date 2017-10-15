8b6f25e8937e9aabbeb783838dbc57076be7f8c5
twyrostek <tpwvyc@mail.missouri.edu>
2017-10-15 02:45:53 -0500

    changes to queries.php

--- studentTimes.php
+++ studentTimes.php
@@ -36,26 +36,7 @@
 				break;
 			}
 
-			if($stmt = mysqli_prepare($link, "SELECT id FROM Users WHERE name = ?") or die ("prepare error" . mysqli_error($link))){
-				mysqli_stmt_bind_param($stmt, "s", $student) or die ("bind param" . mysqli_stmt_error($stmt));
 
-				if(mysqli_stmt_execute($stmt) or die ("not executed")){
-					mysqli_stmt_store_result($stmt) or die (mysqli_stmt_error($stmt));
-
-					if(mysqli_stmt_num_rows($stmt) == 0){
-						if($stmtCreateStudent = mysqli_prepare($link, "INSERT INTO Users (name) VALUES (?)") or die ("prepare error" . mysqli_error($link))){
-							mysqli_stmt_bind_param($stmtCreateStudent, "s", $student) or die ("bind param" . mysqli_stmt_error($stmtCreateStudent));
-							mysqli_stmt_execute($stmtCreateStudent) or die(mysqli_stmt_error($stmtCreateStudent));
-
-							if(!mysqli_affected_rows($link)){								
-								echo json_encode(array("error" => "Could not create resources for " . $student));	
-							}
-						}
-
-						mysqli_stmt_close($stmtCreateStudent);
-					}		
-				}
-			}
 
 			mysqli_stmt_close($stmt);
 
