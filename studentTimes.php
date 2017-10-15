a8338d5f5407acc80cfe492c842b21ee55bcb5c1
FireLemons <ShenYang64@gmail.com>
2017-10-15 01:18:24 -0500

    1st query finished

--- studentTimes.php
+++ studentTimes.php
@@ -26,8 +26,8 @@
 
 	ini_set('session.gc_maxlifetime', 86400);
 
-	$action = (empty($_POST["action"])) ? "" : htmlspecialchars($_POST["action"]);
-	$student = (empty($_POST["student"])) ? "" : htmlspecialchars($_POST["student"]);
+	$action = (empty($_POST["action"])) ? "" : mysqli_real_escape_string($link, htmlspecialchars($_POST["action"]));
+	$student = (empty($_POST["student"])) ? "" : mysqli_real_escape_string($link, htmlspecialchars($_POST["student"]));
 
 	switch ($action) {
 		case "set":
@@ -37,8 +37,28 @@
 			}
 
 			if($stmt = mysqli_prepare($link, "SELECT id FROM Users WHERE name = ?") or die ("prepare error" . mysqli_error($link))){
+				mysqli_stmt_bind_param($stmt, "s", $student) or die ("bind param" . mysqli_stmt_error($stmt));
 
+				if(mysqli_stmt_execute($stmt) or die ("not executed")){
+					mysqli_stmt_store_result($stmt) or die (mysqli_stmt_error($stmt));
+
+					if(mysqli_stmt_num_rows($stmt) == 0){
+						if($stmtCreateStudent = mysqli_prepare($link, "INSERT INTO Users (name) VALUES (?)") or die ("prepare error" . mysqli_error($link))){
+							mysqli_stmt_bind_param($stmtCreateStudent, "s", $student) or die ("bind param" . mysqli_stmt_error($stmtCreateStudent));
+							mysqli_stmt_execute($stmtCreateStudent) or die(mysqli_stmt_error($stmtCreateStudent));
+
+							if(!mysqli_affected_rows($link)){								
+								echo json_encode(array("error" => "Could not create resources for " . $student));	
+							}
+						}
+
+						mysqli_stmt_close($stmtCreateStudent);
+					}		
+				}
 			}
+
+			mysqli_stmt_close($stmt);
+
 			break;
 		case "reset":
 			if(!$student){
